7c759ee7bf766d86420e083cba73181c27b71261
Sneezedip <97994282+Sneezedip@users.noreply.github.com>
2023-01-03 15:23:12 -0100

    Add files via upload

---
+++ Password Generator.py
@@ -0,0 +1,43 @@
+
+import string
+import random
+import time
+
+def choice():
+    global n
+    print('''
+    How much digits do you want in your password (Including digits!)
+    ''')
+    n = int(input(": "))
+    if n >=200 :
+        print("Too big. Try a smaller number")
+        choice()
+    elif n <= 5:
+        print("Too Small. Try a bigger number")
+        choice()
+    else:
+        config()
+
+def config():
+    global ran_str
+    ran_str = ''.join(random.choices(string.ascii_uppercase +string.ascii_lowercase+ string.digits, k = n))
+    final()
+
+def final():
+    print("Random String : " + str(ran_str))
+    time.sleep(0.5)
+    save = input("Would you like to save the password? (y/n)")
+    if save == "y" or save == "Y":
+        opt = input("What program are you going to use the password? ")
+        with open("password.txt", mode = "a") as f:
+            f.write("\n")
+            f.write(f"{opt} - Your password: {ran_str}")
+        print(f"Ok saved")
+        input("")
+    elif save == "n" or save == "N":
+        print("Ok")
+    else:
+        print("Unknown command.")
+        final()
+    
+choice()
